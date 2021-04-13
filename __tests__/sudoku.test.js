cb126307e183e5208336fd7aea6d04422b2b3c1c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-13 14:47:49 -0700

    WIP commit

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -8,27 +8,11 @@ describe('SudoRow1', () => {
   });
 
   it('should create an object with a row array containing nine keys', () => {
-    expect(reusableSudoRow1.row1[0]).toEqual(1);
-    expect(reusableSudoRow1.row1[1]).toEqual(2);
-    expect(reusableSudoRow1.row1[2]).toEqual(3);
-    expect(reusableSudoRow1.row1[3]).toEqual(4);
-    expect(reusableSudoRow1.row1[4]).toEqual(5);
-    expect(reusableSudoRow1.row1[5]).toEqual(6);
-    expect(reusableSudoRow1.row1[6]).toEqual(7);
-    expect(reusableSudoRow1.row1[7]).toEqual(8);
-    expect(reusableSudoRow1.row1[8]).toEqual(9);
+    expect(reusableSudoRow1.row1[0].toEqual(1));
   });
 
   it('should check to see that a row array contains numbers', () => {
-    expect(reusableSudoRow1.row1[0]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[1]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[2]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[3]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[4]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[5]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[6]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[7]).toEqual(expect.any(Number));
-    expect(reusableSudoRow1.row1[8]).toEqual(expect.any(Number));
+    expect(reusableSudoRow1.verify).toEqual(expect.any(Number));
   });
 
   
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,3 +1,11 @@
 export default function SudoRow1(obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19) {
   this["row1"] = [obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19];
 };
+
+SudoRow1.prototype.verify = function() {
+  sudoRow1.forEach(function() {
+    if (Number.isNaN(sudoRow1.row1[i])) {
+      return "not a number";
+    };
+  });
+};
