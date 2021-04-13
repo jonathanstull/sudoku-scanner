f3a0969aaacebbef3259b30783258cc3aadce124
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-13 14:07:03 -0700

    reframe first test using an object array

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,19 +1,26 @@
 import SudoRow1 from './../src/sudoku.js';
 
 describe('SudoRow1', () => {
+  let reusableSudoRow1;
 
-  it('should create a row object with nine keys', () => {
-    const sudoRow1 = new SudoRow(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    expect(sudoRow1.obj11).toEqual(1);
-    expect(sudoRow1.obj12).toEqual(2);
-    expect(sudoRow1.obj13).toEqual(3);
-    expect(sudoRow1.obj14).toEqual(4);
-    expect(sudoRow1.obj15).toEqual(5);
-    expect(sudoRow1.obj16).toEqual(6);
-    expect(sudoRow1.obj17).toEqual(7);
-    expect(sudoRow1.obj18).toEqual(8);
-    expect(sudoRow1.obj19).toEqual(9);
+  beforeEach(() => {
+    reusableSudoRow1 = new SudoRow1(1, 2, 3, 4, 5, 6, 7, 8, 9);
   });
+
+  it('should create an object with a row array containing nine keys', () => {
+    expect(sudoRow1[0]).toEqual(1);
+    expect(sudoRow1[1]).toEqual(2);
+    expect(sudoRow1[2]).toEqual(3);
+    expect(sudoRow1[3]).toEqual(4);
+    expect(sudoRow1[4]).toEqual(5);
+    expect(sudoRow1[5]).toEqual(6);
+    expect(sudoRow1[6]).toEqual(7);
+    expect(sudoRow1[7]).toEqual(8);
+    expect(sudoRow1[8]).toEqual(9);
+  });
+
+  // it('should check to see that a row object has numbers in it', () => {
+
 });
 
 // describe('Sudoku', () => {
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,11 +1,3 @@
 // export default function SudoRow1(obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19) {
-//   this.obj11 = obj11;
-//   this.obj12 = obj12;
-//   this.obj13 = obj13;
-//   this.obj14 = obj14;
-//   this.obj15 = obj15;
-//   this.obj16 = obj16;
-//   this.obj17 = obj17;
-//   this.obj18 = obj18;
-//   this.obj19 = obj19;
+//   this.row1 = [obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19];
 // };
